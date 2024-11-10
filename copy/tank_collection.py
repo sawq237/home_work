7bbc7f741ab18c7ef1409b6c2abe7dc868d9ed4e
streaxxx69 <cerkas132@gmail.com>
2024-11-10 16:11:20 +0300

    fff

--- copy/tank_collection.py
+++ copy/tank_collection.py
@@ -11,12 +11,10 @@ def initialize(canv):
     _canvas = canv
     player = Tank(canvas=canv, x=100, y=50, ammo=100, speed=1, bot=False)
     enemy = Tank(canvas=canv, x=300, y=300, ammo=100, speed=1, bot=True)
-    neutral = Tank(canvas=canv, x=300, y=300, ammo=100, speed=1, bot=False)
-    neutral.stop()
     enemy.set_target(player)
 
     _tanks.append(player)
-    _tanks.append(enemy)
+    # _tanks.append(enemy)
     print(_tanks)
 
 
@@ -35,19 +33,17 @@ def check_collisions(tank):
             continue
         if tank.intersects(other_tank):
             return True
-        return False
+    return False
 
 
 def spawn_enemy():
     while True:
-       pos_x = randint(200, 800)
-       pos_y  = randint(200, 800)
-
-
-    t = Tank(_canvas, x=pos_x, y=pos_y, speed=1)
-    if not check_collisions(t):
-        t.set_target(get_player())
-        _tanks.append(t)
-        return True
+        pos_x = randint(200, 800)
+        pos_y  = randint(200, 800)
+        t = Tank(_canvas, x=pos_x, y=pos_y, speed=1)
+        if not check_collisions(t):
+            t.set_target(get_player())
+            _tanks.append(t)
+            return True
 
 
