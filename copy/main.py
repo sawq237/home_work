deb803e5bcc5cd2c81711c70015f9ec57b8549f9
streaxxx69 <cerkas132@gmail.com>
2024-11-27 19:13:25 +0300

    ддд

--- copy/main.py
+++ copy/main.py
@@ -1,11 +1,8 @@
-# Позиционирование камеры на танке игрока
-
-from tank import Tank
 from tkinter import*
 # 1 подключение библиотеки world
 import world
 import tank_collection
-
+import texture
 
 
 KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN = 37, 39, 38, 40
@@ -20,15 +17,14 @@ FPS = 60
 def update():
     # 1 будем переставлять камеру в новые координаты совпадфющие с координатами танка игрока
     # world.set_camera_xy(player.get_x(), player.get_y())
-    tank_collection.update()
-
     player = tank_collection.get_player()
-
     # 2 отцентруем камеру
-    world.set_camera_xy(player.get_x()-world.SCREEN_WIDTH//2 + player.get_size()//2,
-                        player.get_y()-world.SCREEN_HEIGHT//2 + player.get_size()//2)
-    # check_collision()
+    world.set_camera_xy(player.get_x() - world.SCREEN_WIDTH // 2 + player.get_size() // 2,
+                        player.get_y() - world.SCREEN_HEIGHT // 2 + player.get_size() // 2)
+    tank_collection.update()
+    world.update_map()
     w.after(1000//FPS, update)
+
 
 def key_press(event):
     player = tank_collection.get_player()
@@ -48,21 +44,33 @@ def key_press(event):
         world.move_camera(-5, 0)
     elif event.keycode == KEY_RIGHT:
         world.move_camera(5, 0)
-
     elif event.keycode == 32:
         tank_collection.spawn_enemy()
+
+def load_textures():
+    texture.load('tank_up', 'img/tankT34_up.png')
+    texture.load('tank_down', 'img/tankT34_down.png')
+    texture.load('tank_left', 'img/tankT34_left.png')
+    texture.load('tank_right', 'img/tankT34_right.png')
+
+    texture.load(world.BRICK, 'img/brick.png')
+    texture.load(world.WATER, 'img/water.png')
+    texture.load(world.CONCRETE, 'img/wall.png')
+
+
 
 
 
 w = Tk()
+load_textures()
 w.title('Танки на минималках 2.0')
-canv = Canvas(w, width = world.WIDTH, height = world.HEIGHT, bg = 'alice blue')
+canv = Canvas(w, width = world.WIDTH, height = world.HEIGHT, bg ='#8ccb5e')
 canv.pack()
-
-
+world.initialize(canv)
 
 
 tank_collection.initialize(canv)
+
 w.bind('<KeyPress>', key_press)
 
 
--- copy/world.py
+++ copy/world.py
@@ -1,3 +1,6 @@
+import texture
+from tkinter import NW
+from random import randint, choice
 
 _camera_x = 0
 _camera_y = 0
@@ -7,6 +10,7 @@ SCREEN_HEIGHT = 800
 
 WIDTH = SCREEN_WIDTH * 6
 HEIGHT = SCREEN_HEIGHT * 4
+_map = []
 
 def set_camera_xy(x, y):
     global _camera_x, _camera_y
@@ -16,13 +20,56 @@ def set_camera_xy(x, y):
     if y < 0:
         y = 0
 
-    if x > WIDTH - SCREEN_WIDTH:
-        x = WIDTH - SCREEN_WIDTH
-    if y > HEIGHT - SCREEN_HEIGHT:
-        y = HEIGHT - SCREEN_HEIGHT
+    if x > get_width() - SCREEN_WIDTH:
+        x = get_width() - SCREEN_WIDTH
+    if y > get_height() - SCREEN_HEIGHT:
+        y = get_height() - SCREEN_HEIGHT
 
     _camera_x = x
     _camera_y = y
+
+GROUND = 'g'
+WATER = 'w'
+CONCRETE = 'c'
+BRICK = 'b'
+
+_canvas = None
+BLOCK_SIZE = 64
+
+
+def get_rows():
+    return len(_map)
+
+
+def get_cols():
+    return len(_map[0])
+
+
+def get_width():
+    return get_cols() * BLOCK_SIZE
+
+
+def get_height():
+    return get_rows() * BLOCK_SIZE
+def initialize(canv):
+    global _canvas
+    _canvas = canv
+    create_map(20, 20)
+
+def create_map(rows=20, cols=20):
+    global _map
+    _map = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            if i == 0 or j == 0 or i == rows - 1 or j == cols - 1:
+                block = CONCRETE
+            else:
+                block = choice([BRICK, WATER, GROUND]) if randint(1, 100) <= 15 else GROUND
+
+            cell = _Cell(_canvas, block, j * BLOCK_SIZE, i * BLOCK_SIZE)
+            row.append(cell)
+        _map.append(row)
 
 def move_camera(delta_x, delta_y):
     set_camera_xy(_camera_x + delta_x, _camera_y + delta_y)
@@ -32,3 +79,41 @@ def get_screen_x(world_X):
 
 def get_screen_y(world_Y):
     return world_Y - _camera_y
+
+def update_map():
+    for i in range(0, get_rows()):
+        for j in range(0, get_cols()):
+            update_cell(i, j)
+def update_cell(row, col):
+    if row < 0 or col < 0 or row >= get_rows() or col >= get_cols():
+        return
+    _map[row][col].update()
+
+
+class _Cell:
+    def __init__(self, canvas, block, x, y):
+        self.__canvas = canvas
+        self.__block = block
+        self.__x = x
+        self.__y = y
+        self.__create_element(block)
+
+    def __create_element(self, block):
+        if block != GROUND:
+            self.__id = self.__canvas.create_image(self.__x, self.__y, image=texture.get(block), anchor=NW)
+
+    def __del__(self):
+        try:
+            self.__canvas.delete(self.__id)
+        except:
+            pass
+
+    def get_block(self):
+        return self.__block
+
+    def update(self):
+        if self.__block == GROUND:
+            return
+        screen_x = get_screen_x(self.__x)
+        screen_y = get_screen_y(self.__y)
+        self.__canvas.moveto(self.__id, x=screen_x, y=screen_y)
